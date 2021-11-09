2d51cf776ec6350a169ba026810246cb6e10d790
Teja2633 <93144694+Teja2633@users.noreply.github.com>
2021-11-09 12:39:00 +0530

    Add files via upload

---
+++ teja .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+
+     char ch;
+     printf("Enter a character (a-z or A-Z)");
+     scanf("%c", &ch);
+     printf("Corresponding ASCII number  for %c is %d", ch, ch);
+ }
+
+
+
+	
+	
+	
+	
+
